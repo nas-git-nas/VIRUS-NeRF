e8c5f57dd339325f715c66943fcfbd8a5575345b
Nicolaj Schmid <nicolaj.schmid@epfl.ch>
2023-10-26 18:00:43 +0200

    writing occupancy grid

---
+++ modules/occupancy_grid.py
@@ -0,0 +1,240 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+from args.args import Args
+
+class OccupancyGrid():
+    def __init__(
+        self,
+        args:Args,
+        grid_size:int,
+    ) -> None:
+        self.args = args
+        self.grid_size = grid_size
+
+        self.grid = 0.5 * torch.ones(grid_size, grid_size, grid_size, device=args.device, dtype=torch.float32)
+
+        self.cell_size = 2*self.args.model.scale / grid_size
+
+
+        self.std_floor = 0.06 # minimum standard deviation of sensor model
+
+    def rayUpdate(
+        self,
+        rays_o:torch.Tensor,
+        rays_d:torch.Tensor,
+        meas:torch.Tensor,
+    ):
+        """
+        Update grid with ray measurement.
+        Args:
+            rays_o: rays origins; tensor (N, 3)
+            rays_d: rays directions; tensor (N, 3)
+            meas: measured distance in cube coordinates; tensor (N, 1)
+        """
+        # calculate probabilities for each cell
+        cell_idxs, probs_occ, probs_emp = self._rayProb(
+            rays_o=rays_o,
+            rays_d=rays_d,
+            meas=meas,
+        ) # (N*M, 3), (N*M,), (N*M,)
+
+        # update grid
+        self._updateGrid(
+            cell_idxs=cell_idxs,
+            probs_occ=probs_occ,
+            probs_emp=probs_emp,
+        )
+
+    def _updateGrid(
+        self,
+        cell_idxs:torch.Tensor,
+        probs_occ:torch.Tensor,
+        probs_emp:torch.Tensor,
+    ):
+        """
+        Update grid with probabilities.
+        Args:
+            cell_idxs: indices of cells to update; tensor (N*M, 3)
+            probs_occ: probabilities of measurements given cell is occupied; tensor (N*M,)
+            probs_emp: probabilities of measurements given cell is empty; tensor (N*M,)
+        """
+        probs = self.grid[cell_idxs[:, 0], cell_idxs[:, 1], cell_idxs[:, 2]] # (N*M,)
+        probs = (probs * probs_occ) / (probs * probs_occ + (1 - probs) * probs_emp) # (N*M,)
+        self.grid[cell_idxs[:, 0], cell_idxs[:, 1], cell_idxs[:, 2]] = probs
+
+    def _rayProb(
+        self,
+        rays_o:torch.Tensor,
+        rays_d:torch.Tensor,
+        meas:torch.Tensor,
+    ):
+        """
+        Update grid with ray measurement.
+        Args:
+            rays_o: rays origins; tensor (N, 3)
+            rays_d: rays directions; tensor (N, 3)
+            meas: measured distance in cube coordinates; tensor (N,)
+        Returns:
+            cell_idxs: indices of cells to update; tensor (N*M, 3)
+            probs_occ: probabilities of measurements given cell is occupied; tensor (N*M,)
+            probs_emp: probabilities of measurements given cell is empty; tensor (N*M,)
+        """
+        N = meas.shape[0]
+        M = 16
+
+        # calculate cell distances
+        stds = self._calcStd(
+            dists=meas,
+        ) # (N,)
+        steps = torch.linspace(0, 1, M, device=self.args.device, dtype=torch.float32) # (M,)
+        cell_dists = steps[None,:] * (meas + 3*stds)[:,None] # (N, M)
+        
+
+        # calculate cell probabilities
+        probs_occ, probs_emp = self._rayMeasProb(
+            meas=meas, 
+            dists=cell_dists,
+        )  # (N, M), (N, M)
+        probs_occ = probs_occ.reshape(-1) # (N*M,)
+        probs_emp = probs_emp.reshape(-1) # (N*M,)
+
+        # calculate cell indices
+        cell_pos = rays_o[:, None, :] + rays_d[:, None, :] * cell_dists[:, :, None]  # (N, M, 3)
+        cell_idxs = self._c2idx(
+            pos=cell_pos.reshape(-1, 3),
+        ) # (N*M, 3)
+
+        return cell_idxs, probs_occ, probs_emp
+
+    def _rayMeasProb(
+        self,
+        meas:torch.Tensor,
+        dists:torch.Tensor,
+        return_probs:bool=False,
+    ):
+        """
+        Compute probability of measurement given cell is occupied or empty
+        P[meas@dist | cell=occ] and P[meas@dist | cell=emp] where
+        P[meas@dist | cell] = P[meas=dist | cell] * P[meas not< dist | cell]
+            -> P[meas=dist | cell=occ]: sensor model e.g. accuracy of a 
+                measurement at a given distance
+            -> P[meas=dist | cell=emp]: probability of false detection
+            -> P[meas not< dist | cell]: probability of not detecting any 
+        Args:
+            meas: measured distance in cube coordinates; tensor (N,)
+            dists: distances to evaluate; tensor (N, M)
+            return_probs: whether to return all probabilities; bool
+        Returns:
+            probs_occ: probabilities of measurements given cell is occupied; tensor (N, M)
+            probs_emp: probabilities of measurements given cell is empty; tensor (N, M)
+        """
+        I = 32 # number of samples for integral
+
+        # calculating P[meas=dist | cell=emp] and P[meas=dist | cell=occ]
+        probs_equal_emp = self._sensorEmptyPDF(
+            shape=dists.shape,
+        ) # (N, M)
+        probs_equal_occ = probs_equal_emp + self._sensorOccupiedPDF(
+            meas=meas[:, None],
+            dists=dists,
+        ) # (N, M)
+
+        # calculating P[meas not< dist | cell=emp] and P[meas not< dist | cell=occ]
+        probs_notless_emp = 1 - probs_equal_emp * dists # (N, M)
+        y = torch.linspace(0, 1, I)[None, :] * meas[:, None] # (N, I)
+        integral = self._sensorOccupiedPDF(
+            meas=y[:, None, :],
+            dists=dists[:, :, None],
+        )
+        integral = torch.sum(integral, dim=2) * (meas/I)[:, None] # (N, M)
+        probs_notless_occ = probs_notless_emp - integral # (N, M)
+
+        # calculating P[meas@dist | cell=emp] and P[meas@dist | cell=occ]
+        probs_emp = probs_equal_emp * probs_notless_emp
+        probs_occ = probs_equal_occ * probs_notless_occ
+
+        if return_probs:
+            return probs_occ, probs_emp, probs_equal_emp, probs_equal_occ, probs_notless_emp, probs_notless_occ
+        return probs_occ, probs_emp
+    
+    def _sensorEmptyPDF(
+        self,
+        shape:tuple,
+    ):
+        """
+        Calculate empty probability density function of sensor model:
+        Probabilty that measurement equals to distance given
+        that the cell is empty: P[meas=dist | cell=emp].
+        Args:
+            shape: shape of tensor to return; tuple
+        Returns:
+            probs: probabilities of measurements given cell is empty; tensor (shape)
+        """
+        return 0.2 * torch.ones(shape)
+    
+    def _sensorOccupiedPDF(
+        self,
+        meas:torch.Tensor,
+        dists:torch.Tensor,
+    ):
+        """
+        Calculate occupied probability density function of sensor model:
+        Probabilty that measurement equals to distance given
+        that the cell is occupied: P[meas=dist | cell=occ].
+        Args:
+            meas: measured distance in cube coordinates; tensor (any shape)
+            dists: distances to evaluate; tensor (same shape as meas)
+        Returns:
+            probs: probabilities of measurements given cell is occupied; tensor (same shape as meas)
+        """
+        attenuations = self._calcAttenuation(
+            dists=dists,
+        )
+        stds = self._calcStd(
+            dists=dists,
+        )
+        return attenuations * torch.exp(-0.5 * (meas - dists)**2 / stds**2)
+    
+    def _calcStd(
+        self,
+        dists:torch.Tensor,
+    ):
+        """
+        Calculate standard deviation of sensor model.
+        Args:
+            dists: distances to evaluate; tensor (any shape)
+        Returns:
+            stds: standard deviation of sensor model; tensor (same shape as dists)
+        """
+        return self.std_floor * ( 1 + dists)
+    
+    def _calcAttenuation(
+        self,
+        dists:torch.Tensor,
+    ):
+        """
+        Calculate attenuation of sensor model.
+        Args:
+            dists: distances to evaluate; tensor (any shape)
+        Returns:
+            attenuations: attenuation of sensor model; tensor (same shape as dists)
+        """
+        return 0.6 * (1 - torch.minimum(torch.ones_like(dists), 0.25*dists))
+
+    def _c2idx(
+        self,
+        pos:torch.Tensor,
+    ):
+        """
+        Convert cube coordinates to occupancy grid indices.
+        Args:
+            pos: position in cube coordinates; tensor (N, 2/3)
+        Returns:
+            idx: occupancy grid indices; tensor (N, 2/3)
+        """
+        map_idxs = (self.grid_size - 1) * (pos + self.args.model.scale) / (2 * self.args.model.scale) # (N, x y z)
+        return torch.clamp(map_idxs.round().to(dtype=torch.int32), 0, self.grid_size-1) # convert to int
+
+
