237c34255e6798477be08a13c8c1b57adafaebd4
Nicolaj Schmid <nicolaj.schmid@epfl.ch>
2023-11-29 07:39:43 +0100

    move to fct

--- helpers/system_fcts.py
+++ helpers/system_fcts.py
@@ -1,4 +1,8 @@
 import sys
+import gc
+import torch
+import numpy as np
+
 
 def get_size(
     obj, 
@@ -31,3 +35,55 @@ def get_size(
     elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
         size += sum([get_size(i, seen) for i in obj])
     return size
+
+def moveToRecursively(
+    obj,
+    destination:str,
+    depth:int=0,
+    max_depth:int=6,
+):
+    """
+    Move object recursively to cpu, delete it and free its memory.
+    Args:
+        obj: object to free its memory
+        destination: destination device; str
+        depth: current depth of recursion; int
+        max_depth: maximum depth of recursion; int
+    """
+    if depth > max_depth:
+        return
+    depth += 1
+
+    if hasattr(obj, "to"):
+        obj.to(destination)
+        return
+    
+    if isinstance(obj, np.ndarray):
+        return
+    
+    if hasattr(obj, "__dict__"):
+        for sub_obj in obj.__dict__.values():
+            moveToRecursively(
+                obj=sub_obj,
+                destination=destination,
+                depth=depth,
+                max_depth=max_depth,
+            )
+    
+    if isinstance(obj, dict):
+        for sub_obj in obj.values():
+            moveToRecursively(
+                obj=sub_obj,
+                destination=destination,
+                depth=depth,
+                max_depth=max_depth,
+            )
+
+    if isinstance(obj, list) or isinstance(obj, tuple):
+        for sub_obj in obj:
+            moveToRecursively(
+                obj=sub_obj,
+                destination=destination,
+                depth=depth,
+                max_depth=max_depth,
+            )
